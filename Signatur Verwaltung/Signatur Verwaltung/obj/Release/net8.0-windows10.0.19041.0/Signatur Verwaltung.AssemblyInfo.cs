1c350b30479c7b2f4e0a8980b37d8eacbc08caff
Marc Büttner <mb@mkjc.net>
2026-06-18 17:38:59 +0200

    Update 5.0.0.1

--- Signatur Verwaltung/Signatur Verwaltung/obj/Release/net8.0-windows10.0.19041.0/Signatur Verwaltung.AssemblyInfo.cs
+++ Signatur Verwaltung/Signatur Verwaltung/obj/Release/net8.0-windows10.0.19041.0/Signatur Verwaltung.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("©2025 MIBUE.NET Ltd & Marc Büttner")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee96bce69583ce3459cc449a8dfb2559d12deda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e830b88929a6b4778e51209d577b88a145be3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Signatur Verwaltung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Signatur Verwaltung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("5.0.0")]
